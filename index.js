e8372dae11a8b9ad81c7ba2123975400fd5246d7
AhmetYasarKaplan <e210503060@stud.tau.edu.tr>
2024-09-10 23:46:42 +0300

    create post route to add exercise and get route to get exercises

--- index.js
+++ index.js
@@ -55,11 +55,64 @@ app.post("/api/users", async (req, res) => {
   }
 });
 
-app.post("/api/users/:_id/exercises", (req, res) => {
+app.post("/api/users/:_id/exercises", async (req, res) => {
   const id = req.params._id;
   const description = req.body.description;
   const duration = req.body.duration;
   const date = req.body.date;
+
+  const exerciseDate = date ? new Date(date) : new Date();
+
+  try {
+    const result = await db.query(
+      "INSERT INTO exercises (user_id, description, duration, date) VALUES ($1, $2, $3, $4) RETURNING *",
+      [id, description, duration, exerciseDate]
+    );
+    const queryUsername = await db.query(
+      "SELECT username FROM users WHERE user_id = $1",
+      [id]
+    );
+    res.json({
+      id: result.rows[0].user_id,
+      username: queryUsername.rows[0].username,
+      date: exerciseDate.toDateString(),
+      duration: result.rows[0].duration,
+      description: result.rows[0].description,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const id = req.params._id;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+
+  let query = "SELECT * FROM logs WHERE user_id = $1";
+  let queryParams = [id];
+
+  //TODO: Add from, to, limit functionality to query
+  // if (limit) {
+  //   query += " LIMIT $2";
+  //   queryParams.push(limit);
+  // }
+  console.log(queryParams);
+
+  try {
+    const result = await db.query(query, queryParams);
+    res.json({
+      _id: result.rows[0].user_id,
+      username: result.rows[0].username,
+      count: result.rows[0].count,
+      log: result.rows[0].log,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
